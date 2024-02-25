668a48140ef691bf42a61e480436a1ba808ac6c1
GioVincentLabao <70188174+giovincentlabaoadamson@users.noreply.github.com>
2024-02-25 16:50:37 +0800

    Add files via upload

---
+++ TBA_MLP_Lab4/code1.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+cap = cv.VideoCapture(0)
+while True:
+    ret, img = cap.read()
+    if not ret:
+        break
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img_blur = cv.GaussianBlur(img_gray,(7,7),0)
+
+    sobelx = cv.Sobel(src=img_blur, ddepth=cv.CV_64F, dx=1, dy=0, ksize=5)
+    filtered_image_x = cv.convertScaleAbs(sobelx)
+    sobely = cv.Sobel(src=img_blur, ddepth=cv.CV_64F, dx=0, dy=1, ksize=5)
+    filtered_image_y = cv.convertScaleAbs(sobely)
+    sobelxy = cv.Sobel(src=img_blur, ddepth=cv.CV_64F, dx=1, dy=1, ksize=5)
+    filtered_image_xy = cv.convertScaleAbs(sobelxy)
+
+    plt.figure(figsize=(20,20))
+
+    plt.subplot(221),plt.imshow(img),plt.title('Original')
+    plt.axis("off")
+    plt.subplot(222),plt.imshow(filtered_image_x, cmap = "gray"),plt.title('Filtered Image x-axis')
+    plt.axis("off")
+    plt.subplot(223),plt.imshow(filtered_image_y, cmap = "gray"),plt.title('Filtered Image y-axis')
+    plt.axis("off")
+    plt.subplot(224),plt.imshow(filtered_image_xy, cmap = "gray"),plt.title('Filtered Image xy-axis')
+    plt.axis("off")
+
+    plt.draw()
+    plt.pause(0.01)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    plt.show()
+
+    cap.release
+    cv.destroyAllWindows()
---
+++ TBA_MLP_Lab4/code2.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+cap = cv.VideoCapture(0)
+while True:
+    ret, img = cap.read()
+    if not ret:
+        break
+
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img_blur = cv.GaussianBlur(img_gray, (7, 7), 0)
+    edges = cv.Canny(img_gray,100,200)
+    filtered_img = cv.convertScaleAbs(edges)
+
+    plt.subplot(121),plt.imshow(img),plt.title('Original')
+    plt.axis("off")
+
+    plt.subplot(122),plt.imshow(edges,cmap='gray'),plt.title('Canny')
+    plt.axis("off")
+    plt.show()
+
+    plt.draw()
+    plt.pause(0.01)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    plt.show()
+
+    cap.release
+    cv.destroyAllWindows()
